ee36a3404bf052732f4b4a8e0aae159aed815c5e
nghia <assassincreed2k1@gmail.com>
2025-10-06 12:00:00 +0700

    update login and dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('pages.dashboard'); // Cập nhật đường dẫn mới
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
