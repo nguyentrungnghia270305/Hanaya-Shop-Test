da4e2742a0811325899dfec7202318022d81e4a6
nghia <assassincreed2k1@gmail.com>
2025-10-08 12:00:00 +0700

    add data

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Tạo 20 user giả
+        User::factory()->count(20)->create();
+    }
+}
