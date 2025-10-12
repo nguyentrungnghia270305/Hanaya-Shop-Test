24aa88d1745cddf4273287fd5200f16e975531c1
nghia <assassincreed2k1@gmail.com>
2025-10-12 12:00:00 +0700

    fix ui: Change location component file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,18 +19,19 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
+            {{-- @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endisset --}}
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
+            @include('components.home.footer')
         </div>
     </body>
 </html>
