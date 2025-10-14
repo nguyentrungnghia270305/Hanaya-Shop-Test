4d5ae418b2331b6e60eee725fb608bd662a42973
Nezuko1909 <kny1909.nezuko@gmail.com>
2025-10-14 12:00:00 +0700

    Add route admin

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ Route::get('/souvenir', function () {
     return view('pages.souvenir');
 })->name('souvenir');
 
+Route::get('/adminPage', function() {
+    return view('pages.adminPage');
+})->name('adminPage');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
