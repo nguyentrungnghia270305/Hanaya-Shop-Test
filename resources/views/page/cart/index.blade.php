1fc1bb960e24eeea76a733783af09015b835bcd3
nghia <assassincreed2k1@gmail.com>
2025-10-21 12:00:00 +0700

    Fix Bugs: Can not sort other categories

--- resources/views/page/cart/index.blade.php
+++ resources/views/page/cart/index.blade.php
@@ -110,7 +110,7 @@
                     @endforeach
                     <div class="bg-white rounded shadow p-4 flex justify-between items-center font-bold">
                         <span>Tổng cộng:</span>
-                        <span id="totalPrice">0₫</span>
+                        <span id="totalPrice">0 USD</span>
                     </div>
                     <div class="text-right mt-4">
                         <button type="submit" class="bg-pink-600 hover:bg-pink-700 text-white px-6 py-2 rounded shadow">
